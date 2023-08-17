6670c158c8522ab1e39a5b0de79c383857298158
Anurag Singh Chauhan <dev.anuragchauhan@gmail.com>
2023-08-17 18:59:38 +0530

    fix: @mui/base error removed

--- src/app/(main)/test/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from "@mui/base";
-import React from "react";
-
-function page() {
-  return <Button className="bg-white text-red-300">Hello world</Button>;
-}
-
-export default page;
